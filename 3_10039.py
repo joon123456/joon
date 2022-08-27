cd5281ef4c19f4e1353c21e6359eddc9179da4e6
joon123456 <110708476+joon123456@users.noreply.github.com>
2022-08-27 13:07:40 +0900

    Create 3_10039.py

---
+++ 3_10039.py
@@ -0,0 +1,26 @@
+a=int(input())
+if a >=40:
+  a=a
+else:
+  a=int(40)
+b=int(input())
+if b >=40:
+  b=b
+else:
+  b=int(40)
+c=int(input())
+if c >=40:
+  c=c
+else:
+  c=int(40)
+d=int(input())
+if d >=40:
+  d=d
+else:
+  d=int(40)
+e=int(input())
+if e >=40:
+  e=e
+else:
+  e=int(40)
+print(int((a+b+c+d+e)/5))
