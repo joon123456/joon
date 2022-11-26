468629dabbeeebfea39b3ba47e17e97a29cbaae5
joon123456 <110708476+joon123456@users.noreply.github.com>
2022-11-26 12:57:29 +0900

    Create 0012_3003.py

---
+++ 0012_3003.py
@@ -0,0 +1,31 @@
+a,b,c,d,e,f=input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+d=int(d)
+e=int(e)
+f=int(f)
+if a!=1:
+  print(1-a,end=' ')
+else:
+ print(0,end=' ')
+if b!=1:
+  print(1-b,end=' ')
+else:
+ print(0,end=' ')
+if c!=2:
+  print(2-c,end=' ')
+else:
+ print(0,end=' ')
+if d!=2:
+  print(2-d,end=' ')
+else:
+ print(0,end=' ')
+if e!=2:
+  print(2-e,end=' ')
+else:
+ print(0,end=' ')
+if f!=8:
+  print(8-f,end=' ')
+else:
+ print(0,end=' ')
