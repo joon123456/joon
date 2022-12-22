7ff25d679c360647772d21b463008045ed0924fe
joon123456 <110708476+joon123456@users.noreply.github.com>
2022-12-23 00:18:29 +0900

    Create 0015_2372.py

---
+++ 0015_2372.py
@@ -0,0 +1,7 @@
+print('Animal      Count')
+print('-----------------')
+print('Chickens      100')
+print("Clydesdales     5")
+print("Cows           40")
+print("Goats          22")
+print("Steers          2")
