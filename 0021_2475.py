e4390e745a2cb78391c88aab88afe272d9f4891d
joon123456 <110708476+joon123456@users.noreply.github.com>
2023-04-29 16:33:10 +0900

    Update 0021_2475.py

--- 0021_2475.py
+++ 0021_2475.py
@@ -2,3 +2,14 @@ def ver(a,b,c,d,e):
   return((a*a+b*b+c*c+d*d+e*e)%10)
 a,b,c,d,e,= map(int,input().split())
 print(ver(a,b,c,d,e))
+
+#or
+
+def ver(data):
+  sum = 0
+  for i in data:
+    sum = sum + i * i
+  return (sum%10)
+
+data = list(map(int, input().split()))
+print(ver(data))
